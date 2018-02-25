ee46b83121aa45f9bb7057342a758350851b96f1
Koki Makino <kokiseikomakino@gmail.com>
2018-02-26 00:46:44 +0900

    able to obtain access token --not complete

--- app/src/main/java/edu/self/githubapp/afterlogin.java
+++ app/src/main/java/edu/self/githubapp/afterlogin.java
@@ -2,6 +2,7 @@ package edu.self.githubapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -37,51 +38,71 @@ public class afterlogin extends AppCompatActivity {
                 TextView txtview = (TextView)findViewById(R.id.textview);
                 txtview.setText(code);
 
-                /* アクセストークンを入手するためにアクセス*/
-                String clientid = "55567c2247acc9d37bbf";
-                String clientsecret = "4e245a455b80a65b507bd87f09c2313e6a8f1077";
-                String urlStr = "https://github.com/login/oauth/access_token?client_id=" + clientid + "&client_secret=" + clientsecret +"&code" + code ;
-                //Uri uri1 = Uri.parse(urlStr);
-                //Intent obtain_token = new Intent(Intent.ACTION_VIEW,uri1);
-                //startActivity(obtain_token);
-                HttpURLConnection con = null;
-                InputStream is = null;
-                String result = "";
-
-                try{
-                    URL url = new URL(urlStr);
-                    con = (HttpURLConnection)url.openConnection();
-                    con.setRequestMethod("POST");
-                    con.setRequestProperty("Accept", "application/json");
-                    con.connect();
-                    is = con.getInputStream();
-                    result = IOUtils.toString(is, StandardCharsets.UTF_8);
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                finally {
-                    if(con != null){
-                        con.disconnect();
-                    }
-                    if(is != null){
-                        try{
-                            is.close();
-                        }
-                        catch (IOException e){
-
-                        }
-                    }
-                }
-
-                txtview.setText(result);
+                TextView txtview2 = (TextView)findViewById(R.id.textview2);
+                accesstokenreceiver receiver = new accesstokenreceiver(txtview2);
+                receiver.execute(code);
 
             }
         }
+
     }
 
+    private class accesstokenreceiver extends AsyncTask<String, String, String>{
+        private TextView _act;
+        public accesstokenreceiver(TextView act){
+            _act = act;
+        }
+
+        public String doInBackground(String... params){
+            String code = params[0];
+
+            /* アクセストークンを入手するためにアクセス*/
+            String clientid = "55567c2247acc9d37bbf";
+            String clientsecret = "4e245a455b80a65b507bd87f09c2313e6a8f1077";
+            String urlStr = "https://github.com/login/oauth/access_token?client_id=" + clientid + "&client_secret=" + clientsecret +"&code=" + code  ;
+            //Uri uri1 = Uri.parse(urlStr);
+            //Intent obtain_token = new Intent(Intent.ACTION_VIEW,uri1);
+            //startActivity(obtain_token);
+            HttpURLConnection con = null;
+            InputStream is = null;
+            String result = "";
+
+            try{
+                URL url = new URL(urlStr);
+                con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Accept", "application/json");
+                con.connect();
+                is = con.getInputStream();
+                result = IOUtils.toString(is, StandardCharsets.UTF_8);
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if(con != null){
+                    con.disconnect();
+                }
+                if(is != null){
+                    try{
+                        is.close();
+                    }
+                    catch (IOException e){
+
+                    }
+                }
+            }
+            return result;
+        }
+
+        public void onPostExecute(String result){
+            _act.setText(result);
+
+        }
+    }
 
 
 }
